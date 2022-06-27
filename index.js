b91dbf33eda618396140c26fa22f1111084d382b
Sajeda Saju <sajedasaju85@gmail.com>
2022-06-28 03:25:13 +0600

    cors origin added

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ const app = express()
 
 //middleware
 app.use(cors());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use(express.json());
 
 
